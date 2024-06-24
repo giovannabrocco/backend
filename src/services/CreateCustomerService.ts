cd2e767b8409d166c809b9acb2a76ea009b074eb
giovannabrocco <giovannabroccoluizz@hotmail.com>
2024-06-24 17:13:00 -0300

    execute name e email

--- src/services/CreateCustomerService.ts
+++ src/services/CreateCustomerService.ts
@@ -1,7 +1,12 @@
 import prismaClient from "../prisma";
 
+interface CreateCustomerProps {
+    name: string;
+    email: string
+}
+
 class CreateCustomerService {
-    async execute () {
+    async execute ({name, email}: CreateCustomerProps) {
         console.log("ROTA FOI CHAMADA");
 
         return { ok: true}
